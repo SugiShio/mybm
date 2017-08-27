f9d6b74632c366e65f4bcf4f3743920cf320148d
Shoko SUGITO <fruitmachine0@gmail.com>
2017-08-27 19:38:20 +0900

    コンタクトページ

--- wp-content/themes/mybm/page-contact.php
+++ wp-content/themes/mybm/page-contact.php
@@ -6,9 +6,13 @@
     </nav>
 
     <h2 class="sectionTtl">Contact</h2>
-    <ul>
-        <li class="icon-mail"><a href="mailto:mellowyellowbananamoon@yahoo.co.jp">mellowyellowbananamoon@yahoo.co.jp</a></li>
-        <li><a href="https://twitter.com/mellowyellow877" target="_blank">@mellowyellow877</a></li>
+    <?php if(have_posts()): while(have_posts()): the_post();?>
+        <div class="text"><?php the_content(); ?></div>
+    <?php endwhile; endif; ?>
+
+    <ul class="link">
+        <li class="link__mail"><a href="mailto:mellowyellowbananamoon@yahoo.co.jp">mellowyellowbananamoon@yahoo.co.jp</a></li>
+        <li class="link__twitter"><a href="https://twitter.com/mellowyellow877" target="_blank">@mellowyellow877</a></li>
     </ul>
 </section>
 
