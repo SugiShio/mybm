db18d0480d8e666784085f0be1c21980a2fbf65a
Shoko SUGITO <fruitmachine0@gmail.com>
2017-09-06 22:15:26 +0900

    スケジュール表示順

--- wp-content/themes/mybm/category-schedule.php
+++ wp-content/themes/mybm/category-schedule.php
@@ -12,6 +12,7 @@
             <?php while (have_posts()) : the_post(); ?>
                 <li class="post">
                     <header>
+                        <time><?php echo get_post_time('Y.n.j D'); ?></time>
                         <h3 class="post__ttl"><?php the_title(); ?></h3>
                         <?php if(!empty(post_custom('event URL'))) : ?>
                             <p><a href="<?php echo post_custom('event URL'); ?>">
--- wp-content/themes/mybm/functions.php
+++ wp-content/themes/mybm/functions.php
@@ -4,4 +4,25 @@
     function shortenUrl($url) {
         return preg_replace('/https?:\/\//', '', $url);
     }
- ?>
+
+    //予約投稿機能を無効化
+    add_action('save_post', 'setSchedule', 99);
+    add_action('edit_post', 'setSchedule', 99);
+    function setSchedule() {
+        global $wpdb;
+        if (is_category('schedule')) {
+            $sql = 'UPDATE `'.$wpdb->prefix.'posts` ';
+            $sql .= 'SET post_status = "publish" ';
+            $sql .= 'WHERE post_status = "future"';
+            $wpdb->get_results($sql);
+        }
+    }
+
+    // 日付の近いものから表示
+    add_action('pre_get_posts', 'my_pre_get_posts');
+    function my_pre_get_posts($query) {
+        if (is_category('schedule')) {
+            $query->set('order', 'ASC');
+        }
+    }
+?>
